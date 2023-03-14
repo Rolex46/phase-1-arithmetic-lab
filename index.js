b0bc0663aabd249ad77576bb7b770582e0ba1d7e
Rolex Omondi <rolexomondi46@gmail.com>
2023-03-14 10:57:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
-// Write your code here
+
+
+let max = Math.max(12,20);
+max;
+
+const num3 = 24; 
+const num4 = 5;
+let mod = 24%5;
+
+const num1 = 31;
+const num2 = 2;
+let multiply = 31 * 2;
+
+const number = 1;
+const random = number;
+
+ 
+
+
+
+
+    
+
